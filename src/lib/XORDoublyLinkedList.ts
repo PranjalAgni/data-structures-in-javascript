1e72c0499497e26ad0f0c9b919bbfdcf5d1f681d
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-11-28 19:40:41 +0530

    :sparkles: refactored the code

--- src/lib/XORDoublyLinkedList.ts
+++ src/lib/XORDoublyLinkedList.ts
@@ -1,5 +1,11 @@
-import BidirectionalMap from "./BidirectionalMap";
+import * as crypto from "crypto";
 
+/* eslint-disable no-console */
+/**
+ * You can't implement a proper XOR linked list in JavaScript, because you can't access an object's memory address.
+
+  There's not really a practical reason to do this in JavaScript, either. From what I understand, the only benefit of an XOR linked list is a slightly reduced memory footprint. The amount of memory you would save with such a structure in JavaScript is offset by the overhead of the objects themselves.
+ */
 class XORDLLNode {
   public value: number;
   public xorAddressValue: number;
@@ -12,10 +18,13 @@ class XORDLLNode {
 
 class XORDoublyLinkedList {
   private head: XORDLLNode;
-  private addressMap: BidirectionalMap<XORDLLNode, number>;
+  private addressMap: Map<XORDLLNode, number>;
+  private valueMap: Map<number, XORDLLNode>;
+
   constructor() {
     this.head = null;
-    this.addressMap = new BidirectionalMap<XORDLLNode, number>();
+    this.addressMap = new Map<XORDLLNode, number>();
+    this.valueMap = new Map<number, XORDLLNode>();
   }
 
   createNode(value: number): XORDLLNode {
@@ -23,40 +32,45 @@ class XORDoublyLinkedList {
     return newNode;
   }
 
+  generateAddress(): number {
+    return crypto.randomInt(2, 1000000);
+  }
   getAddressValueFromNode(node: XORDLLNode): number {
-    return this.addressMap.hasKey(node) ? this.addressMap.getByKey(node) : 0;
+    return this.addressMap.has(node) ? this.addressMap.get(node) : 0;
   }
 
   getNodeFromAddressValue(value: number): XORDLLNode {
-    return this.addressMap.hasValue(value)
-      ? this.addressMap.getByValue(value)
-      : null;
+    return this.valueMap.has(value) ? this.valueMap.get(value) : null;
   }
 
-  computeXOR(nodeA: XORDLLNode, nodeB: XORDLLNode): number {
+  computeXOR(nodeA: XORDLLNode, nodeB: XORDLLNode, isPrinting = false): number {
     const addressNodeA = this.getAddressValueFromNode(nodeA);
     const addressNodeB = this.getAddressValueFromNode(nodeB);
     if (!addressNodeA && !addressNodeB)
       throw new Error("Both the nodes are null");
-    console.log(`Operation =  ${addressNodeA} ^ ${addressNodeB}`);
+    if (isPrinting)
+      console.log(
+        `Operation =  ${addressNodeA} ^ ${addressNodeB} = ${
+          addressNodeA ^ addressNodeB
+        }`
+      );
     return addressNodeA ^ addressNodeB;
+  }
+
+  addNodeToMap(node: XORDLLNode, genAddressValue = 0): void {
+    const addressValue = genAddressValue || this.generateAddress();
+    // console.log({ addressValue, genAddressValue });
+    this.addressMap.set(node, addressValue);
+    this.valueMap.set(addressValue, node);
   }
 
   insert(value: number): void {
     const newNode = this.createNode(value);
+    // add this node to Map, so address is generated
+    this.addNodeToMap(newNode);
 
-    /**
-     * n2 = {2, addr(0)} *head
-     * {n2: 1}
-     *
-     * n5 = {5, addr(n2)}
-     * {n2: 1, n5: 2}
-     */
-    const addressValue: number = this.addressMap.size() + 1;
-    this.addressMap.set(newNode, addressValue);
-
+    // returns addressValue or 0 if head is NULL
     newNode.xorAddressValue = this.getAddressValueFromNode(this.head);
-    console.log("Next node address = ", newNode.xorAddressValue);
 
     if (this.head !== null) {
       const currentHead = this.head;
@@ -65,13 +79,21 @@ class XORDoublyLinkedList {
       );
 
       if (nextNode) {
-        const currentHeadXORAddress = this.computeXOR(newNode, nextNode);
-        currentHead.xorAddressValue = currentHeadXORAddress;
-        this.addressMap.set(
-          currentHead,
-          this.getAddressValueFromNode(currentHead)
-        );
-        this.addressMap.set(nextNode, currentHeadXORAddress);
+        const nextNodeAddressValue = this.computeXOR(newNode, nextNode);
+
+        // delete nextNode from addressMap and valueMap
+        this.addressMap.delete(nextNode);
+        this.valueMap.delete(currentHead.xorAddressValue);
+
+        // recreate nextNode with nextNodeAddressValue address in addressMap and valueMap
+        this.addNodeToMap(nextNode, nextNodeAddressValue);
+
+        // delete head from addressMap and valueMap then add it back with nextNodeAddressValue should be attached on the head
+        this.addressMap.delete(currentHead);
+        this.valueMap.delete(newNode.xorAddressValue);
+
+        currentHead.xorAddressValue = nextNodeAddressValue;
+        this.addNodeToMap(currentHead, newNode.xorAddressValue);
       }
     }
 
@@ -79,13 +101,14 @@ class XORDoublyLinkedList {
   }
 
   printAddressMap(): void {
-    this.addressMap.map.forEach((value, key) => {
+    console.log("===============");
+    this.addressMap.forEach((value, key) => {
       console.log(JSON.stringify(key) + " = " + value);
     });
+    console.log("===============");
   }
 
   print(): void {
-    console.log("===============");
     let previousNode: XORDLLNode = null;
     let nextNode: XORDLLNode = null;
     let current: XORDLLNode = this.head;
@@ -96,7 +119,8 @@ class XORDoublyLinkedList {
       nextNode = this.getNodeFromAddressValue(
         this.computeXOR(
           previousNode,
-          this.getNodeFromAddressValue(current.xorAddressValue)
+          this.getNodeFromAddressValue(current.xorAddressValue),
+          true
         )
       );
 
@@ -112,7 +136,8 @@ const xorDLL = new XORDoublyLinkedList();
 
 xorDLL.insert(2);
 xorDLL.insert(5);
-// xorDLL.insert(7);
+xorDLL.insert(7);
+xorDLL.insert(8);
 
 xorDLL.printAddressMap();
 xorDLL.print();
